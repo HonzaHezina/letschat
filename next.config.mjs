502fba099bdce8e10e987089cba2f71ced57c4ee
HonzaHezina <hezina@gmail.com>
2025-06-20 13:20:48 +0200

    Downgrade Next.js to stable version

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false,
-  compiler: {
-    // Disable SWC
-  },
-  experimental: {
-    forceSwcTransforms: false,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
