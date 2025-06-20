0ce548be9c81855d7a38e472b12e74c2485bd3f8
HonzaHezina <hezina@gmail.com>
2025-06-20 13:19:59 +0200

    Disable SWC and use Babel fallback

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
+  compiler: {
+    // Disable SWC
+  },
   experimental: {
-    appDir: true,
+    forceSwcTransforms: false,
   },
 };
 
